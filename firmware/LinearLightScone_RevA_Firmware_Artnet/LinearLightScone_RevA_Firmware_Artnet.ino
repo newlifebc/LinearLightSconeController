c9c9072717e5b56684f9505aa60598bbc87db270
Swarts, Matthew E <strontium5@gmail.com>
2019-08-25 13:17:51 -0400

    added initial firmware

---
+++ firmware/LinearLightScone_RevA_Firmware_Artnet/LinearLightScone_RevA_Firmware_Artnet.ino
@@ -0,0 +1,285 @@
+#include <WiFi.h>
+
+#include <WebServer.h>
+#include <AutoConnect.h>
+#include <AutoConnectCredential.h>
+#include <PageBuilder.h>
+
+#include <WiFiUdp.h>
+#include <ArtnetWifi.h>
+#include <FastLED.h>
+
+
+WebServer Server;
+AutoConnect       Portal(Server);
+String viewCredential(PageArgument&);
+String delCredential(PageArgument&);
+
+
+// Specified the offset if the user data exists.
+// The following two lines define the boundalyOffset value to be supplied to
+// AutoConnectConfig respectively. It may be necessary to adjust the value
+// accordingly to the actual situation.
+
+#define CREDENTIAL_OFFSET 0
+//#define CREDENTIAL_OFFSET 64
+
+
+//Wifi settings - be sure to replace these with the WiFi network that your computer is connected to
+//const char* ssid = "SSID";
+//const char* password = "pAsSwOrD";
+
+/**
+ *  An HTML for the operation page.
+ *  In PageBuilder, the token {{SSID}} contained in an HTML template below is
+ *  replaced by the actual SSID due to the action of the token handler's
+ * 'viewCredential' function.
+ *  The number of the entry to be deleted is passed to the function in the
+ *  POST method.
+ */
+static const char PROGMEM html[] = R"*lit(
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  html {
+  font-family:Helvetica,Arial,sans-serif;
+  -ms-text-size-adjust:100%;
+  -webkit-text-size-adjust:100%;
+  }
+  .menu > a:link {
+    position: absolute;
+    display: inline-block;
+    right: 12px;
+    padding: 0 6px;
+    text-decoration: none;
+  }
+  </style>
+</head>
+<body>
+<div class="menu">{{AUTOCONNECT_MENU}}</div>
+<form action="/del" method="POST">
+  <ol>
+  {{SSID}}
+  </ol>
+  <p>Enter deleting entry:</p>
+  <input type="number" min="1" name="num">
+  <input type="submit">
+</form>
+</body>
+</html>
+)*lit";
+
+static const char PROGMEM autoconnectMenu[] = { AUTOCONNECT_LINK(BAR_24) };
+
+// URL path as '/'
+PageElement elmList(html,
+  {{ "SSID", viewCredential },
+   { "AUTOCONNECT_MENU", [](PageArgument& args) {
+                            return String(FPSTR(autoconnectMenu));} }
+  });
+PageBuilder rootPage("/", { elmList });
+
+// URL path as '/del'
+PageElement elmDel("{{DEL}}", {{ "DEL", delCredential }});
+PageBuilder delPage("/del", { elmDel });
+
+// Retrieve the credential entries from EEPROM, Build the SSID line
+// with the <li> tag.
+String viewCredential(PageArgument& args) {
+  AutoConnectCredential  ac(CREDENTIAL_OFFSET);
+  struct station_config  entry;
+  String content = "";
+  uint8_t  count = ac.entries();          // Get number of entries.
+
+  for (int8_t i = 0; i < count; i++) {    // Loads all entries.
+    ac.load(i, &entry);
+    // Build a SSID line of an HTML.
+    content += String("<li>") + String((char *)entry.ssid) + String("</li>");
+  }
+
+  // Returns the '<li>SSID</li>' container.
+  return content;
+}
+
+// Delete a credential entry, the entry to be deleted is passed in the
+// request parameter 'num'.
+String delCredential(PageArgument& args) {
+  AutoConnectCredential  ac(CREDENTIAL_OFFSET);
+  if (args.hasArg("num")) {
+    int8_t  e = args.arg("num").toInt();
+    if (e > 0) {
+      struct  station_config entry;
+
+      // If the input number is valid, delete that entry.
+      int8_t  de = ac.load(e - 1, &entry);  // A base of entry num is 0.
+      if (de > 0) {
+        ac.del((char *)entry.ssid);
+
+        // Returns the redirect response. The page is reloaded and its contents
+        // are updated to the state after deletion. It returns 302 response
+        // from inside this token handler.
+        Server.sendHeader("Location", String("http://") + Server.client().localIP().toString() + String("/"));
+        Server.send(302, "text/plain", "");
+        Server.client().flush();
+        Server.client().stop();
+
+        // Cancel automatic submission by PageBuilder.
+        delPage.cancel();
+      }
+    }
+  }
+  return "";
+}
+
+
+
+
+
+
+
+// LED Strip
+#define NUM_LEDS 60
+#define FRAMES_PER_SECOND  120
+const int numLeds = NUM_LEDS; // Change if your setup has more or less LED's
+const int numberOfChannels = numLeds * 3; // Total number of DMX channels you want to receive (1 led = 3 channels)
+#define DATA_PIN 13 //The data pin that the WS2812 strips are connected to.
+CRGB leds[numLeds];
+
+// Artnet settings
+ArtnetWifi artnet;
+const int startUniverse = 0;
+
+bool sendFrame = 1;
+int previousDataLength = 0;
+
+/*
+// connect to wifi – returns true if successful or false if not
+boolean ConnectWifi(void)
+{
+  boolean state = true;
+  int i = 0;
+
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20){
+      state = false;
+      break;
+    }
+    i++;
+  }
+  if (state){
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("");
+    Serial.println("Connection failed.");
+  }
+
+  return state;
+}
+*/
+
+void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
+{
+  sendFrame = 1;
+  // set brightness of the whole strip 
+  if (universe == 15)
+  {
+    FastLED.setBrightness(data[0]);
+  }
+  // read universe and put into the right part of the display buffer
+  for (int i = 0; i < length / 3; i++)
+  {
+    int led = i + (universe - startUniverse) * (previousDataLength / 3);
+    if (led < numLeds)
+    {
+      leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+    }
+  }
+  previousDataLength = length;     
+  FastLED.show();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  rootPage.insert(Server);    // Instead of Server.on("/", ...);
+  delPage.insert(Server);     // Instead of Server.on("/del", ...);
+
+  
+  // Enable saved past credential by autoReconnect option,
+  // even once it is disconnected.
+  //Config.autoReconnect = true;
+  
+  // Set an address of the credential area.
+  AutoConnectConfig Config;
+  uint64_t chipid = ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+  String chipidstr = "";
+  for(int i=0;i<6;i++){
+    chipidstr+=String((uint8_t)(chipid>>40-i*8),HEX);
+  }
+  chipidstr.toUpperCase();
+  Serial.println(chipidstr);
+  Config.apid = "scone-"+chipidstr; //change the ssid
+  Config.psk = "";//no password
+  Config.boundaryOffset = CREDENTIAL_OFFSET;
+  Portal.config(Config);
+
+  // Start
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+  }
+  
+  
+  //ConnectWifi();
+  artnet.begin();
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, numLeds);
+
+  // onDmxFrame will execute every time a packet is received by the ESP32
+  artnet.setArtDmxCallback(onDmxFrame);
+}
+
+
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+static uint8_t gHue = 0;
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+
+void loop()
+{
+  // we call the read function inside the loop
+  artnet.read();
+
+  Portal.handleClient();
+
+  
+  //Serial.print("x");
+  sinelon();
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 200 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+}
